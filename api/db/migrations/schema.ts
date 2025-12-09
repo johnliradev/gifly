9c4cc919a791035fe0eca22cb116e80f2cea3090
John <johnalsoares@gmail.com>
2025-12-09 12:25:39 -0300

    feat (api): add initial database schema and relations for users

---
+++ api/db/migrations/schema.ts
@@ -0,0 +1,15 @@
+import { pgTable, unique, serial, varchar, timestamp } from "drizzle-orm/pg-core"
+import { sql } from "drizzle-orm"
+
+
+
+export const users = pgTable("users", {
+	id: serial().primaryKey().notNull(),
+	name: varchar({ length: 255 }).notNull(),
+	email: varchar({ length: 255 }).notNull(),
+	passwordHash: varchar("password_hash", { length: 255 }).notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow().notNull(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).defaultNow().notNull(),
+}, (table) => [
+	unique("users_email_unique").on(table.email),
+]);
